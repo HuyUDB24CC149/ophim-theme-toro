584e844940efaad67d4b62eff41f4c6d8c3bd76c
HuyUDB24CC149 <HuyUD.B24CC149@hotmail.com>
2025-03-30 20:28:06 +0700

    Delete resources directory

--- resources/views/themetoro/inc/rightbar.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- $tops is in src/ThemeToroServiceProvider.php --}}
-<aside>
-    @foreach($tops as $top)
-        @include("themes::themetoro.inc.rightbar." . $top['template'])
-    @endforeach
-</aside>
